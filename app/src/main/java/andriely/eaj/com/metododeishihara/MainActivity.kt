3c96122f2ed7dab8ab866d00d06b0ee8d104b8f2
mraasf <mraasf@gmail.com>
2018-08-08 23:15:29 -0300

    23:15

--- app/src/main/java/andriely/eaj/com/metododeishihara/MainActivity.kt
+++ app/src/main/java/andriely/eaj/com/metododeishihara/MainActivity.kt
@@ -3,7 +3,6 @@ package andriely.eaj.com.metododeishihara
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -16,22 +15,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela01)
 
-            val Teste1 = findViewById(R.id.BtnTeste01)
-        
+        val Teste1 = findViewById(R.id.BtnTeste01);
+        val Teste2 = findViewById(R.id.BtnTeste02)
+        val Teste3 = findViewById(R.id.BtnTeste03)
 
 
-            val intent = Intent(applicationContext, tela02::class.java)
+        val intent = Intent(applicationContext, tela02::class.java)
 
 
 
 
         Teste1(View v) {
 
-            Intent intent = new Intent(getApplicationContext(),tela02.class);
+            Intent intent = new Intent(getApplicationContext(), tela02.class);
             // Altera a imagem do imageview
-             val imagem03 = findViewById(R.id.imageView) as ImageView
+            val imagem01 = findViewById(R.id.imageView) as ImageView
+            imagem01.setImageResource(R.drawable.teste1)
+        }
+        Teste2(View v) {
+
+            Intent intent = new Intent(getApplicationContext(), tela02.class);
+            // Altera a imagem do imageview
+            val imagem03 = findViewById(R.id.imageView) as ImageView
+            imagem03.setImageResource(R.drawable.teste2)
+        }
+
+        Teste3(View v) {
+
+            Intent intent = new Intent(getApplicationContext(), tela02.class);
+            // Altera a imagem do imageview
+            val imagem03 = findViewById(R.id.imageView) as ImageView
             imagem03.setImageResource(R.drawable.teste3)
         }
 
 
+    }
 }
